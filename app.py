80e86149e9d73471bd78e4ace9280c530f1d7dd8
Achiwilms <philip901203777@gmail.com>
2023-08-04 13:41:27 +0800

    handle sticker

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, StickerMessage
 
 from langchain.memory import MongoDBChatMessageHistory
 from src.build_LLMChain import build_chain
@@ -43,9 +43,9 @@ def callback():
         abort(400)
     return 'OK'
 
-# handle message
+# handle text message
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
+def handle_text_message(event):
     # user ID
     user_id = event.source.user_id
     # message
@@ -68,7 +68,13 @@ def handle_message(event):
         reply = chain_response(chain, mongodb_message_history, msg)
     # send reply to user
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply))
-    
+
+# handle sticker message
+@handler.add(MessageEvent, message=StickerMessage)
+def handle_sticker_message(event):
+    print(event.message)
+    reply = "我看不懂貼圖 U+1F605"
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply))
 
 # make url discoverable
 @app.route("/", methods=['GET'])
